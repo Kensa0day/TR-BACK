dc796391d39be68cc2374a15b381c785d4feb2cd
KenSa0day <108104817+Kensa0day@users.noreply.github.com>
2023-11-22 20:44:03 +0700

    Create connectMongo.js

---
+++ connectMongo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log('Mongo Connect')
+
+    } catch (err) {
+        console.log('Connect Failed')
+
+    }
+}
+
+module.exports = connectDB
