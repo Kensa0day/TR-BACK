c6d4f4abbb2f09813afe74cb78d5b227c6ab02c7
KenSa0day <108104817+Kensa0day@users.noreply.github.com>
2023-11-22 20:58:14 +0700

    Update connectMongo.js

--- connectMongo.js
+++ connectMongo.js
@@ -1,15 +1,30 @@
+// import mongoose from 'mongoose';
+
+// const connectDB = async() => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI)
+
+//         console.log('Mongo Connect')
+
+//     } catch (err) {
+//         console.log('Connect Failed')
+
+//     }
+// }
+
+// module.exports = connectDB
+
+// connectMongo.js
+
 import mongoose from 'mongoose';
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-
-        console.log('Mongo Connect')
-
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Mongo Connected');
     } catch (err) {
-        console.log('Connect Failed')
-
+        console.error('Connection to MongoDB failed:', err.message);
     }
-}
+};
 
-module.exports = connectDB
+export default connectDB;
