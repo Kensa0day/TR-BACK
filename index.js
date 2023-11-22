5bfcb3e962e20f7c030e994b7043792ae6f9c9df
KenSa0day <108104817+Kensa0day@users.noreply.github.com>
2023-11-22 15:30:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,11 +30,7 @@ app.get('/', (req, res) => {
 mongoose.set("strictQuery", false);
 const connect = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-
-        })
+        await mongoose.connect(process.env.MONGO_URI)
 
         console.log('Mongo Connect')
 
